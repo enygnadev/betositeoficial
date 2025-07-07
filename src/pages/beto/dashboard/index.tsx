baa221bc5336a10c2edda5290f55f757ed783744
devblulab <116671864+devblulab@users.noreply.github.com>
2025-07-07 15:43:51 -0300

    Update index.tsx

--- src/pages/beto/dashboard/index.tsx
+++ src/pages/beto/dashboard/index.tsx
@@ -1052,6 +1052,7 @@ const Dashboard = () => {
                         <span>{doc.nomevendedor}</span>
                         <span>•</span>
                         <DateRange fontSize="small" />
+                        <span>{doc.produtosSelecionados}</span>
                         <span>{formatDate(doc.dataCriacao)}</span>
                         <Thumbnails urls={doc.imagemUrls} />
                       </div>
