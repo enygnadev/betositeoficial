6ff60f9749bf5e4e5469caeafe338907039ccfda
devblulab <116671864+devblulab@users.noreply.github.com>
2025-07-07 15:41:38 -0300

    Update index.tsx

--- src/pages/beto/dashboard/digital/index.tsx
+++ src/pages/beto/dashboard/digital/index.tsx
@@ -16,6 +16,7 @@ import {
 } from '@material-ui/icons';
 import { Timestamp } from 'firebase/firestore';
 import { query, where } from 'firebase/firestore';
+import {Thumbnails} from '@/components/enterprises/betodespa/transferencia/thumbnails';
 
 
 // Configuração do Firebase
@@ -1071,6 +1072,7 @@ const Dashboard = () => {
                         <span>•</span>
                         <DateRange fontSize="small" />
                         <span>{formatDate(doc.dataCriacao)}</span>
+                        <Thumbnails urls={doc.imagemUrls} />
                       </div>
                       <Box mt={1}>
                         <Typography variant="body2" color="textSecondary">
